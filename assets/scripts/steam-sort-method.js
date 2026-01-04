5c396f943a36cc1a342b45bdb2d582063983b34a
Daniella <107291896+DaniLionn@users.noreply.github.com>
2026-01-04 14:33:34 -0700

    borse update

--- assets/scripts/steam-sort-method.js
+++
@@ -1,7 +0,0 @@
-//Changes the way the games on the "Steam games I own" section are sorted.
-
-function sortMethod(sortType) {
-  var steamFrame = document.getElementById("steamgames-iframe");
-  steamFrame.src =
-    "https://dani-backend-apis.onrender.com/website/steamgames?sort=" + sortType;
-}
--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -15,3 +15,12 @@ function playSound(id, doRandomSpeed) {
     audio.play();
   }
 }
+
+//Changes the way the games on the "Steam games I own" section are sorted.
+
+function sortMethod(sortType) {
+  var steamFrame = document.getElementById("steamgames-iframe");
+  steamFrame.src =
+    "https://dani-backend-apis.onrender.com/website/steamgames?sort=" +
+    sortType;
+}
